842ae94227a19d3e73acd8580dae95ffd972ada8
Tetiana Snytko <lisitsina070295@gmail.com>
2020-05-22 00:33:22 +0300

    initial commit 2 Comments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
+    //1) Write a simple Java program using any text editor. Compile this program using Java compiler and run it from the console.
+
     public static void main (String[] args){
         System.out.println("Goodbye planet");
+
+//        2) Write a program which creates and initializes all primitive data types.
 
         byte a = 15;
         System.out.println(a);
@@ -27,16 +31,24 @@ public class Main {
         float h = 12.8f;
         System.out.println(h);
 
-        System.out.println("\u0048\u0065\u006C\u006C\u006F");
+//        5) Read about ASCII. Write unicodes for all characters in a word "Hello".
+
+                System.out.println("\u0048\u0065\u006C\u006C\u006F");
+
+//        6) Create a simple program with several constants.
 
         final int JOY = 5;
 //        JOY = 10;
+
+//        7) Create an array of 10 integers. Initialize this array with any values except 0.
+//        Print the length of the array to a console.
 
         int[] k = new int[10];
         for(int i=0; i<k.length; i++) {
             k[i] = i+1;
             System.out.println(k[i]);
         }
+//        8) Create an array of 5 doubles. Get the first and the last element of the array an write them to variables.
 
         double[] l = new double[5];
         for(int i=0; i<l.length; i++) {
