aeb8ba8335168707912db4a4e0ffcf6f6c782e9e
tetianasnytko <lisitsina070295@gmail.com>
2020-07-23 19:15:08 +0300

    Task 7, second part Implementation

--- src/TV.java
+++ src/TV.java
@@ -1,36 +1,36 @@
 public class TV extends TVDevice {
     @Override
     public void powerOn() {
-
+        System.out.println("Power ON");
     }
 
     @Override
     public void powerOff() {
-
+        System.out.println("Power OFF");
     }
 
     @Override
     public void setChannel(int channel) {
-
+        System.out.println("1+1");
     }
 
     @Override
     public void currentChannel() {
-
+        System.out.println("Netflix");
     }
 
     @Override
     public void upVolume() {
-
+        System.out.println("Louder");
     }
 
     @Override
     public void downVolume() {
-
+        System.out.println("Quieter");
     }
 
     @Override
     public void mute() {
-
+        System.out.println("On mute");
     }
 }
